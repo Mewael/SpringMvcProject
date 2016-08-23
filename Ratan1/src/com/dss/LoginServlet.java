0c1835786c3338d31547aee0705749def83561dc
Mewael Zere <meweneho@yahoo.com>
2016-08-23 17:01:45 -0400

    Initial check in.

---
+++ Ratan1/src/com/dss/LoginServlet.java
@@ -0,0 +1,52 @@
+package com.dss;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.setContentType("text/html");
+	PrintWriter writer = response.getWriter();
+	writer.println("This is my frist application");
+	
+	String name= request.getParameter("uname");
+	String pwd= request.getParameter("upwd");
+	
+	if(name.equals("ratan")&& pwd.equals("ratan")){
+		writer.println("you have loged in successfully");
+		writer.println("user name " + name + "and password " + pwd);
+	}
+	else{
+		writer.println("attempt unsuccessful");
+	}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
