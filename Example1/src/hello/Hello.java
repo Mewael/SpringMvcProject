efedce6c28e4a8926423df7b7539083cc8207702
meweneho <meweneho@yahoo.com>
2016-08-24 17:18:39 -0400

    addition from

---
+++ Example1/src/hello/Hello.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+	public boolean lengthTest(String name){
+		if(name.length()==5)
+		return true;
+		else
+			return false;
+	}
+	
+	public int add(int a, int b){
+		return a+b;
+	}
+
+}
